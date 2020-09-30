1a597b9983a810344b8d71dff6a2e56bb7df642a
arturjzapater <zapater.artur@gmail.com>
2020-09-30 17:24:02 +0200

    moved router to index

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import {
-  BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom'
 import Home from '../features/Home/Home'
+import type { State } from '../types/State'
 
-const App: React.FC = () => (
-  <Router>
+const App: React.FC = () => {
+  const nickname = useSelector((state: State) => state.nickname)
+
+  return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/chatroom" />
     </Switch>
-  </Router>
-)
+  )
+}
 
 export default App
--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -1,14 +1,7 @@
 import React from 'react'
-import type { RouteComponentProps } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import type { State } from '../../types/State'
 import Login from '../Login/Login'
 
-const Home: React.FC<RouteComponentProps> = ({ history }) => {
-  const nickname = useSelector((state: State) => state.nickname)
-
-  if (nickname !== '') history.push('/chatroom')
-
+const Home: React.FC = () => {
   return (
     <Login />
   )
