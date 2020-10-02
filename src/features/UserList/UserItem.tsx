2f564062cf0648836498c235b24e571f1da5a5b7
arturjzapater <zapater.artur@gmail.com>
2020-10-02 11:53:58 +0200

    removed joined information from user list

--- src/features/UserList/UserItem.tsx
+++ src/features/UserList/UserItem.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import type { UserItem as UserItemProps } from '../../types/State'
 
-const UserItem: React.FC<UserItemProps> = ({ nickname, joined, isTyping }) => (
+const UserItem: React.FC<UserItemProps> = ({ nickname, isTyping }) => (
   <li className="mx-4 my-1 break-words leading-tight">
     <span>{nickname}</span>
-    <span>{isTyping ? '(...)' : ''}</span>
-    <span className="text-sm italics">(joined {new Date(joined).toUTCString()})</span>
+    <span>{isTyping ? ' (...)' : ''}</span>
   </li>
 )
 
